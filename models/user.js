9085947110f45b4036a53b45b02db63d894023c7
DeastyAjie <deasty_ajie_32rpl@student.smktelkom-mlg.sch.id>
2025-09-07 22:54:37 +0700

    First Commit

---
+++ models/user.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    _Id: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    imageUrl: { type: String, required: true },
+    cartItems: { type: Object, default: {} },
+}, {minimize: false})
+
+const User = mongoose.models.user || mongoose.model('user', UserSchema);
+
+export default User;
